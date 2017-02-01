48b7e438588d01f181c36d64d0a3ebca7482f486
StevenIseki <stevenisekimartin@gmail.com>
2017-02-02 00:08:35 +1100

    v1.1.3

--- src/audio/AudioButton.js
+++ src/audio/AudioButton.js
@@ -29,8 +29,9 @@ export default class extends Component {
 
   render () {
     return (
-      <svg width='24' height='24' onClick={::this.onClick} className='ld-button-audio'>
-        <path fill='currentColor' d='M10 9v6l5-3-5-3zm8.222-3H5.778C4.8 6 4 6.6 4 7.333v9.334C4 17.4 4.8 18 5.778 18h12.444C19.2 18 20 17.4 20 16.667V7.333C20 6.6 19.2 6 18.222 6z' fillRule='evenodd' />
+      <svg fill="currentColor" className='ld-button-audio' onClick={::this.onClick} height="24" viewBox="0 0 24 24" width="24" >
+        <path d="M8 5v14l11-7z" />
+        <path d="M0 0h24v24H0z" fill="none" />
       </svg>
     )
   }
